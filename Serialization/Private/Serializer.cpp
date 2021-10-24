9559da19d156a486ff9680a28d8c2c05a4c3329f
Cihan Sakarya <cihansakaryacs@gmail.com>
2021-10-24 23:48:49 +0300

    refactor: Move functions to cpp

--- Serialization/Private/Serializer.cpp
+++ Serialization/Private/Serializer.cpp
@@ -1,2 +1,51 @@
 #include "Serializer.h"
 
+std::vector<char> RTCL::Serializer::SerializeByEnumType(SerializedType enumType, void* pointer)
+{
+	return serializeFunctions[static_cast<int>(enumType)](pointer);
+}
+
+std::vector<char> RTCL::Serializer::SerializeInt(void* pointer)
+{
+	return SerializeBySize(pointer, sizeof(int));
+}
+
+std::vector<char> RTCL::Serializer::SerializeLong(void* pointer)
+{
+	return SerializeBySize(pointer, sizeof(long));
+}
+
+std::vector<char> RTCL::Serializer::SerializeFloat(void* pointer)
+{
+	return SerializeBySize(pointer, sizeof(float));
+}
+
+std::vector<char> RTCL::Serializer::SerializeDouble(void* pointer)
+{
+	return SerializeBySize(pointer, sizeof(double));
+}
+
+std::vector<char> RTCL::Serializer::SerializeCharPointer(void* pointer)
+{
+	auto** const recoveredValue = (char**)pointer;
+
+	return SerializeBySize(*recoveredValue, std::strlen(*recoveredValue));
+}
+
+std::vector<char> RTCL::Serializer::SerializeString(void* pointer)
+{
+	auto* const recoveredValue = static_cast<std::string*>(pointer);
+
+	return SerializeBySize((void*)recoveredValue->c_str(), recoveredValue->length());
+}
+
+std::vector<char> RTCL::Serializer::SerializeBySize(void* pointer, size_t size)
+{
+	std::vector<char> data(size);
+
+	auto* charPointer = static_cast<char* const>(pointer);
+
+	std::copy(charPointer, charPointer + size, data.begin());
+
+	return data;
+}
--- Serialization/Public/Serializer.h
+++ Serialization/Public/Serializer.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "SerializedTypeEnum.h"
+#include "SerializedTypeSelector.h"
+
+#include <vector>
+#include <cstring>
 
 namespace RTCL
 {
@@ -8,11 +11,29 @@ namespace RTCL
 	{
 	public:
 
-
+		static std::vector<char> SerializeByEnumType(SerializedType enumType, void* pointer);
 
 	private:
 
+		static std::vector<char> SerializeInt(void* pointer);
+		static std::vector<char> SerializeLong(void* pointer);
+		static std::vector<char> SerializeFloat(void* pointer);
+		static std::vector<char> SerializeDouble(void* pointer);
+		static std::vector<char> SerializeCharPointer(void* pointer);
+		static std::vector<char> SerializeString(void* pointer);
 
+		static std::vector<char> SerializeBySize(void* pointer, size_t size);
 
+		using SerializeFunctionType = std::vector<char>(*)(void*);
+
+		static constexpr SerializeFunctionType serializeFunctions[6] =
+		{
+			SerializeInt,
+			SerializeLong,
+			SerializeFloat,
+			SerializeDouble,
+			SerializeCharPointer,
+			SerializeString
+		};
 	};
 }
